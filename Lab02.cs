bbfba9c172e40088dc8e1500fbad7364656555c1
JTseeker <44356992+JTseeker@users.noreply.github.com>
2018-11-05 00:14:09 -0500

    Add files via upload

---
+++ Lab02.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int number1; // declare first number to multiply
+            int number2; // declare second number to multiply
+            int sum; // declare sum of number1 and number2
+
+            Console.WriteLine("{0}\n{1}", "Hello World", "From Janette");
+            Console.WriteLine("Hello World\tFrom Janette");
+            /* Execution happens when clicking the start key at the top of the screen.  
+             * Integer type variables are used to denote a integer number, while a double is used to denote big floating numbers.  
+             * They also use different key words.
+             * Using the DisplayMessage method, you can create a message and have it displayed on the screen.
+             * A get and A set accessor.
+             * A class is used to house a method.  A object is the outline for the class, showing how it works and what it does.  You can create many instances of a class depending on the instance variables. */
+             
+        } 
+    }
+}
