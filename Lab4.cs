0096e63ce7dd4428068684ba420d098fc531d677
JTseeker <44356992+JTseeker@users.noreply.github.com>
2018-11-19 02:11:23 -0500

    Add files via upload

---
+++ Lab4.cs
@@ -0,0 +1,514 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*There are four basic elements to counter control repetition
+             * One is a control variable.  Another is the initial value of 
+             the control variable.  The increment or the decrement that the control
+             variable is modified.  And the loop continueation condition.
+             
+            What is the difference and simularity between the while and repetition 
+             statments.  The while statement is a iteration statment that allows for the execution
+             of a embedded statment conditionally for zero or more times.
+             It emables the user to control the multiple times in a loop.  It 
+             usually is used when the number of iterations is not known and cannot be
+             determined.  A repetition statment allows the user to specify that a action should be repeated.
+             The difference between the two is that in a repetition statement the 
+             user can determine how many loops are created.  Whiel for a while statment 
+             it is unknown.
+
+            The difference between the while and do while statments are simple.
+            A do while statement evaluates the expression at the bottom, while the ehile statment 
+            evaluates at the top.
+             */
+
+            { for (int i = 1; i < 101; i++)
+                { Console.WriteLine(i); }
+                Console.ReadLine();
+               Console.WriteLine((i % 2) == 0) // it's even
+                Console.WriteLine((i % 1) == 0) // it's odd
+            }
+            { Console.Write("Choose a Temperature");
+                string userValue = Console.ReadLine();
+
+                if (userValue == "10")
+                { string message = "Polar Bear";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "9")
+                {
+                    string message = "Polar Bear";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "8")
+                {
+                    string message = "Polar Bear";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "7")
+                {
+                    string message = "Polar Bear";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "6")
+                {
+                    string message = "Polar Bear";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "5")
+                {
+                    string message = "Polar Bear";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "4")
+                {
+                    string message = "Polar Bear";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "3")
+                {
+                    string message = "Polar Bear";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "2")
+                {
+                    string message = "Polar Bear";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "1")
+                {
+                    string message = "Polar Bear";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "0")
+                {
+                    string message = "Polar Bear";
+                    Console.WriteLine(message);
+                }
+                if (userValue == "20")
+                {
+                    string message = "Penguin";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "19")
+                {
+                    string message = "Penguin";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "18")
+                {
+                    string message = "Penguin";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "17")
+                {
+                    string message = "Penguin";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "16")
+                {
+                    string message = "Penguin";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "15")
+                {
+                    string message = "Penguin";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "14")
+                {
+                    string message = "Penguin";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "13")
+                {
+                    string message = "Penguin";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "12")
+                {
+                    string message = "Penguin";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "11")
+                {
+                    string message = "Penguin";
+                    Console.WriteLine(message);
+                }
+                if (userValue == "40")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "39")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "38")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "37")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "36")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "35")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "34")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "33")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "32")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "31")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "30")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "29")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "28")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "27")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "26")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "25")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "24")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "23")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "22")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "21")
+                {
+                    string message = "Moose";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "20")
+                {
+                    string message = "Reindeer";
+                    Console.WriteLine(message);
+                }
+                if (userValue == "50")
+                {
+                    string message = "Reindeer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "49")
+                {
+                    string message = "Reindeer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "48")
+                {
+                    string message = "Reindeer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "47")
+                {
+                    string message = "Reindeer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "46")
+                {
+                    string message = "Reindeer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "45")
+                {
+                    string message = "Reindeer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "44")
+                {
+                    string message = "Reindeer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "43")
+                {
+                    string message = "Reindeer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "42")
+                {
+                    string message = "Reindeer";
+                    Console.WriteLine(message);
+                }
+                if (userValue == "41")
+                {
+                    string message = "Reindeer";
+                    Console.WriteLine(message);
+                }
+                if (userValue == "60")
+                {
+                    string message = "Deer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "59")
+                {
+                    string message = "Deer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "58")
+                {
+                    string message = "Deer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "57")
+                {
+                    string message = "Deer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "56")
+                {
+                    string message = "Deer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "55")
+                {
+                    string message = "Deer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "54")
+                {
+                    string message = "Deer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "53")
+                {
+                    string message = "Deer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "52")
+                {
+                    string message = "Deer";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "51")
+                {
+                    string message = "Deer";
+                    Console.WriteLine(message);
+                }
+                if (userValue == "70")
+                {
+                    string message = "Turtle";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "69")
+                {
+                    string message = "Turtle";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "68")
+                {
+                    string message = "Turtle";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "67")
+                {
+                    string message = "Turtle";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "66")
+                {
+                    string message = "Turtle";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "65")
+                {
+                    string message = "Turtle";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "64")
+                {
+                    string message = "Turtle";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "63")
+                {
+                    string message = "Turtle";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "62")
+                {
+                    string message = "Turtle";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "61")
+                {
+                    string message = "Turtle";
+                    Console.WriteLine(message);
+                }
+                if (userValue == "80")
+                {
+                    string message = "Lion";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "79")
+                {
+                    string message = "Lion";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "78")
+                {
+                    string message = "Lion";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "77")
+                {
+                    string message = "Lion";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "76")
+                {
+                    string message = "Lion";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "75")
+                {
+                    string message = "Lion";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "74")
+                {
+                    string message = "Lion";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "73")
+                {
+                    string message = "Lion";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "72")
+                {
+                    string message = "Lion";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "71")
+                {
+                    string message = "Lion";
+                    Console.WriteLine(message);
+                }
+                if (userValue == "90")
+                {
+                    string message = "Fish";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "89")
+                {
+                    string message = "Fish";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "88")
+                {
+                    string message = "Fish";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "87")
+                {
+                    string message = "Fish";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "86")
+                {
+                    string message = "Fish";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "85")
+                {
+                    string message = "Fish";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "84")
+                {
+                    string message = "Fish";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "83")
+                {
+                    string message = "Fish";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "82")
+                {
+                    string message = "Fish";
+                    Console.WriteLine(message);
+                }
+                else if (userValue == "81")
+                {
+                    string message = "Fish";
+                    Console.WriteLine(message);
+                }
+                { 
+                for (int i = 10; i < 21; i++)
+                { Console.WriteLine(i);
+                }
+                Console.ReadLine();
+                {
+                    for (int i = 0; i < 101; i++)
+                        Console.WriteLine(i);
+                    Console.WriteLine("********");
+                        /* You would have to use a if else statement for this.*/
+                }
+            }
+        }
+}
